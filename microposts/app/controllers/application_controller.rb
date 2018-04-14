60831e5e33bb6ce91c036167931281218e3251ad
emi <sakahiga@yahoo.co.jp>
2018-04-14 15:43:42 +0000

    user login

--- microposts/app/controllers/application_controller.rb
+++ microposts/app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
+  
+
+  
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  
+  end
+  
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+ 
+  # end
+  
+  # def logged_in?
+  #   !!current_user
+  # end
 end
